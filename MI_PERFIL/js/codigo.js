4b866c71c1c61147afffc7e03ea5557f6dd02d72
JOEL SANGALLI MAMANI <161733666+jhoelsm@users.noreply.github.com>
2024-03-13 23:22:24 -0400

    Add files via upload
    practica de mi perfil en diseño web

---
+++ MI_PERFIL/js/codigo.js
@@ -0,0 +1,39 @@
+function openModal() {
+    document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+/*proyectos*/
+function openModalProyectos() {
+    document.getElementById("modal-proyectos").style.display = "flex";
+}
+
+function closeModalProyectos() {
+    document.getElementById("modal-proyectos").style.display = "none";
+}
+/*habilidades*/
+function openModalHabilidades() {
+    document.getElementById("modal-habilidades").style.display = "flex";
+}
+
+function closeModalHabilidades() {
+    document.getElementById("modal-habilidades").style.display = "none";
+}
+/*hobbies*/
+function openModalHobbies() {
+    document.getElementById("modal-hobbies").style.display = "flex";
+}
+
+function closeModalHobbies() {
+    document.getElementById("modal-hobbies").style.display = "none";
+}
+/*contacto*/
+function openModalContacto() {
+    document.getElementById("modal-contacto").style.display = "flex";
+}
+
+function closeModalContacto() {
+    document.getElementById("modal-contacto").style.display = "none";
+}
